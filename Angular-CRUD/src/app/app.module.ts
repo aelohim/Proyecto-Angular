2bafd6d2144007e327663a3055769e577da5a02f
Aelohim <aelohim2@gmail.com>
2021-10-28 15:40:58 -0300

    rutas de los componentes

--- Angular-CRUD/src/app/app.module.ts
+++ Angular-CRUD/src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+// Componentes
 import { AgregarEmpleadoComponent } from './components/agregar-empleado/agregar-empleado.component';
 import { EditarEmpleadoComponent } from './components/editar-empleado/editar-empleado.component';
 import { ListarEmpleadoComponent } from './components/listar-empleado/listar-empleado.component';
 
+import { SharedModule } from './shared/shared/shared.module';
+
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
   declarations: [
     AppComponent,
     AgregarEmpleadoComponent,
     EditarEmpleadoComponent,
     ListarEmpleadoComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
   providers: [],
+  exports: [
+    AgregarEmpleadoComponent,
+    EditarEmpleadoComponent,
+    ListarEmpleadoComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
